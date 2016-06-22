f0b4f3d02ba4181f573037a3625a2844b8012f8f
stephane-ly <stephane.ly@edu.hefr.ch>
2016-06-22 09:30:22 +0200

    Simple ping function

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <string>
+#include <vector>
+
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    vector<string> online;
+    std::string ip;
+    for(int i=1; i<254; ++i){
+        ip = "192.168.20." + to_string(i);
+        if(!(system(("ping -w 1 " + ip).c_str()))){
+            online.push_back(ip);
+        }
+    }
+    for(int i=0; i<(int)online.size(); ++i){
+        cout << online.at(i) << endl;
+    }
+    return 0;
+}
